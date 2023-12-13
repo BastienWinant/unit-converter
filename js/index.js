8c4df050940ca4fa9074dc27e3bba4fa101d56e4
Armand Winant <145713109+BastienWinant@users.noreply.github.com>
2023-12-13 17:32:40 +0100

    add event listeners and callbacks

---
+++ js/index.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // DOM ELEMENTS
+  const amtInput = document.getElementById("amt-input")
+  const convertBtn = document.getElementById("convert-btn")
+
+  // EVENT LISTENERS
+  convertBtn.addEventListener("click", () => {
+    const inputValue = amtInput.value
+  })
+})
